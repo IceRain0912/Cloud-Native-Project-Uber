59a894d59f1e1c5ab699c63c8a82d4d8b908ec14
anson <ansonwang02.01.25@gmail.com>
2023-12-19 17:05:57 +0800

    fix: book error

--- backend/src/api/Book/GetBook/GetBook.resolvers.ts
+++ backend/src/api/Book/GetBook/GetBook.resolvers.ts
@@ -1,14 +1,42 @@
 import { Resolvers } from '../../../types/resolvers';
+import Book from '../../../entities/Book';
+import {
+  GetBookQueryArgs,
+  GetBookResponse
+} from '../../../types/graph';
 
 const resolvers: Resolvers = {
   Query: {
-    GetBook: async (_, __, context) => {
-      const { req: { book } } = context;
-      return {
-        ok: true,
-        error: null,
-        book
-      };
+    GetBook: async (
+      _,
+      args: GetBookQueryArgs,
+      { req }
+    ): Promise<GetBookResponse> => {
+      try {
+        const book = await Book.findOne({
+          ID: args.BookID
+        });
+
+        if (book) {
+          return {
+            ok: true,
+            error: null,
+            book// Wrap the single Book in an array
+          };
+        } else {
+          return {
+            ok: false,
+            error: 'Book not found',
+            book: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          book: null
+        };
+      }
     }
   }
 }
